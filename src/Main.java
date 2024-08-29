a5f855d1964b58f5440fc9c163d336069d2e9c71
Jānis Vasarietis <janisv@amf.lv>
2024-08-29 14:22:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // for-each = traversing technique to iterate through the elements in an array/collection
+        // less steps, more readable
+        // less flexible
+
+        //String[] animals = {"cat","dog","rat","bird"};
+        ArrayList<String> animals = new ArrayList<String>();
+
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rat");
+        animals.add("bird");
+
+        for(String i : animals) {
+            System.out.println(i);
+        }
+    }
+}
